9f95ae7026f6c83957c352809c2e9cc7069de5e2
samurainisa <samurainisa4@gmail.com>
2025-06-18 16:03:48 +0700

    Добавили в работу иконки

---
+++ AiDetectionFrontend/src/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <i v-if="type === 'material'" class="material-icons" :class="className">{{ name }}</i>
+  <span v-else-if="type === 'outlined'" class="material-symbols-outlined" :class="className">{{ name }}</span>
+  <span v-else-if="type === 'rounded'" class="material-symbols-rounded" :class="className">{{ name }}</span>
+  <i v-else-if="type === 'prime'" class="pi" :class="`pi-${name} ${className}`"></i>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string
+  type?: 'material' | 'outlined' | 'rounded' | 'prime'
+  className?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  type: 'material',
+  className: ''
+})
+</script>
